82fead8438de549a795202138f8414b4070f03a0
djimenezab <djimenez.es@gmail.com>
2026-06-22 17:18:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'regin-v1.5';
+const ASSETS = [
+  '/app-regin/',
+  '/app-regin/index.html',
+  '/app-regin/manifest.json',
+  '/app-regin/icons/icon-192x192.png',
+  '/app-regin/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
